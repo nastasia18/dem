37aedc3a27a45f3c3f9a5329ee1ca12572083424
Anastasia Zavarina <staszavar.2017@gmail.com>
2021-12-26 22:01:51 +0300

    first commit

---
+++ DE/AgentPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using DE.AppData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DE
+{
+    /// <summary>
+    /// Логика взаимодействия для AgentPage.xaml
+    /// </summary>
+    public partial class AgentPage : Page
+    {
+        public AgentPage()
+        {
+            InitializeComponent();
+
+            var History = ConnectDB.conObj.Agent.ToList();
+            ListAgent.ItemsSource = History;
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += UpdateData;
+            timer.Start();
+        }
+
+        public void UpdateData(object sender, object e)
+        {
+            var History = ConnectDB.conObj.Agent.ToList();
+            ListAgent.ItemsSource = History;
+            ListAgent.ItemsSource = ConnectDB.conObj.Agent.Where(p => p.Name.StartsWith(SearchPanel.Text.ToLower()) || p.Phone.StartsWith(SearchPanel.Text) || p.Email.StartsWith(SearchPanel.Text)).ToList();
+        }
+
+        private void TypeSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WSUniversalLib/Calculation.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WSUniversalLib
+{
+    public class Calculation
+    {
+        public int GetQuantityForProduct(int productType, int materialType, int count, float width, float length)
+        {
+            double coefType, coef;
+            if (productType == 1)
+            {
+                coefType = 1.1;
+            }
+            else if (productType == 2)
+            {
+                coefType = 2.5;
+            }
+            else if (productType == 3)
+            {
+                coefType = 8.43;
+            }
+            else
+            {
+                return -1;
+            }
+
+            if (materialType == 1)
+            {
+                coef = 0.3;
+            }
+            else if (materialType == 2)
+            {
+                coef = 0.12;
+            }
+            else
+            {
+                return -1;
+            }
+
+            double countMaterial = count * width * length * coefType;
+            double marriage = countMaterial * coef / 100;
+            double result = countMaterial + marriage;
+            return (int)result;
+        }
+    }
+}
